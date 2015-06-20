4e55b65b661c66171fe0a12588fe8c139b1783b9
Chelsea Du <chsea.du@gmail.com>
2015-06-20 02:48:53 -0400

    add initial game functionality

--- game.js
+++ game.js
@@ -9,22 +9,55 @@ Game.prototype.checkGuess = function(guess) {
     $('body').css('background-color', 'white');
     $('#title').text('Congratulations! You guessed the number!');
     return;
+  } else {
+    this.lives--;
+    if (this.lives == 0) {
+      $('#title').text('Sorry, you\'re out of lives. The number was' + this.secretNum);
+      return;
+    }
+
+    var difference = Math.abs(this.secretNum - guess),
+        response;
+
+    if (difference > 40) {
+      response = 'Ice cold.';
+    } else if (difference > 30) {
+      response = 'Cold.';
+    } else if (difference > 20) {
+      response = 'Warm.';
+    } else if (difference > 10) {
+      response = 'Hot.';
+    } else {
+      response = 'On fire.';
+    }
+    this.guesses.push(guess + ' - ' + response);
+
+    if (this.secretNum > guess) {
+      response += '<br>Guess higher.'
+    } else {
+      response += '<br>Guess lower.'
+    }
+
+    $('#title').html(response);
+
+    $('#guess').val('').focus();
   }
 }
 
 function submit(game) {
   var guess = $('#guess').val();
-
   if (guess >= 1 && guess <= 100) {
-    game.checkGuess(guess);
+    game.checkGuess(+guess);
   } else {
     $('#title').html('Invalid guess!<br>1-100 only!')
     $('#guess').val('').focus();
-  };
+  }
 }
 
 $(document).ready(function() {
   var game = new Game;
+
+  $('#guess').focus();
 
   $('#submit').on('click', function() {
     submit(game);
